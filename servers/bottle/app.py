9a28ffdd3e1420d756d21030d2537d749e1e123e
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-11-12 00:42:54 +0900

    :+1: Add server: bottle(dev)

---
+++ servers/bottle/app.py
@@ -0,0 +1,10 @@
+from bottle import route, run, HTTPResponse
+
+@route('/')
+def app():
+    body = 'Hello world'
+    res = HTTPResponse(status=200, body=body)
+    res.set_header('Content-Type', 'text/plain')
+    return res
+
+run(host='0.0.0.0', port=4000, debug=False)
